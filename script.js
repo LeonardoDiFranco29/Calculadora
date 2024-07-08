291f11ece108a3d971537fed8983bf85de7b32c8
LeonardoDiFranco29 <leonardodanieldifranco@gmail.com>
2024-07-07 22:02:42 -0600

    todo

---
+++ script.js
@@ -0,0 +1,15 @@
+const pantalla = document.getElementById('pantalla');
+
+function verEnPantalla(input){
+    pantalla.value += input;
+}
+function limpiarPantalla(){
+    pantalla.value = "";
+}
+function calcular(){
+    try{
+        pantalla.value = eval(pantalla.value);
+    }catch(error){
+        pantalla.value = "Error";
+    }
+}
